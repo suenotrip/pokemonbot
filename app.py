1110c3e95df378d1c5bae5af3d8263f925520cac
Amit Kumar <amit.iit15@gmail.com>
2016-09-11 21:35:07 +0530

    payment

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def webook():
                 if messaging_event.get('message'):  # someone sent us a message
 
                     sender_id = messaging_event['sender']['id']  # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event['recipient']['id']  # the recipient's ID, which should be your page's facebook ID
+                    #recipient_id = messaging_event['recipient']['id']  # the recipient's ID, which should be your page's facebook ID
                     if 'text' in messaging_event['message']:
                         message_text = messaging_event['message']['text']  # the message's text
                         rules4messages(sender_id,message_text)
