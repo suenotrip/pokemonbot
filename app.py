5fb2381ce63a31e1c76630f4df6443898cd4510a
Amit Kumar <amit.iit15@gmail.com>
2016-09-11 21:54:20 +0530

    payment

--- app.py
+++ app.py
@@ -103,6 +103,8 @@ def webook():
                     log('sender id is ==='+ sender_id + 'payload for postback is ==='+ payload)
                     
                     handlePostback(payload,sender_id)
+    elif data['object']=='stripe' :
+        facebook_id=data['user_id']
         
 
     return ('ok', 200)
